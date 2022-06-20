04315c7c9d6ca14d987fd11cc0f029b3f78f1e30
Dmitry G <sdprecovery@gmail.com>
2022-06-20 23:57:28 +0300

    enum BotState

--- modules/bot.py
+++ modules/bot.py
@@ -1,6 +1,16 @@
-from bdo import Buff, Food, Skill
+from enum import Enum
+
+from bdo import Ability
 from utils import grab_screen
 
 
+class BotState(Enum):
+    INIT = 'INIT'
+    SEARCHING = 'SEARCHING'
+    NAVIGATING = 'NAVIGATING'
+    KILLING = 'KILLING'
+
+
 class BlackDesertBot:
-    pass
+    def __init__(self):
+        pass
