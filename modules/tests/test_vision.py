7d4903f735ba41ef62f57df2e7aab463ab7b3b43
Dmitry G <sdprecovery@gmail.com>
2022-06-21 13:34:09 +0300

    Vision cvt_img_gray debug_imshow test_vision_init test_cvt_img_gray

--- modules/tests/test_vision.py
+++ modules/tests/test_vision.py
@@ -1,16 +1,24 @@
+import cv2 as cv
 import pytest
 
 from unittest import TestCase
 
+from ..utils import grab_screen
 from ..vision import Vision
 
 
 class TestVision(TestCase):
     def setUp(self):
-        pass
+        self.vision = Vision('assets/boar.png')
+        self.vision.debug = True
 
     def test_vision_init(self):
-        vision = Vision('assets/boar.png')
-        assert len(vision.needle_img)
-        assert isinstance(vision.needle_w, int)
-        assert isinstance(vision.needle_h, int)
+        assert isinstance(self.vision.needle_img, object)
+        assert isinstance(self.vision.needle_w, int)
+        assert isinstance(self.vision.needle_h, int)
+
+    def test_cvt_img_gray(self):
+        screen = grab_screen()
+        screen_gray = self.vision.cvt_img_gray(screen)
+        assert isinstance(screen_gray, object)
+        assert isinstance(screen_gray, object)
--- modules/vision.py
+++ modules/vision.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import inspect
 
 from time import time
 
@@ -8,6 +9,7 @@ from .utils import grab_screen
 
 class Vision:
     def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
+        self.debug = False
         self.needle_img, self.needle_w, self.needle_h = self.process_img(needle_img_path)
 
     def process_img(self, img_path: str) -> tuple:
@@ -15,3 +17,17 @@ class Vision:
         img = cv.imread(img_path, 0)
         w, h = img.shape[::-1]
         return (img, w, h)
+
+    def cvt_img_gray(self, img: object, region=None) -> object:
+        """cv2 convert image to gray"""
+        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        self.debug_imshow(inspect.stack()[0][3], img_gray)
+        return img_gray
+
+    def debug_imshow(self, funcname: str, img: object) -> None:
+        """Show cv2 image if debug=True"""
+        if not self.debug:
+            return
+        img = cv.resize(img, (960, 540))
+        cv.imshow(funcname, img)
+        cv.waitKey(500)
